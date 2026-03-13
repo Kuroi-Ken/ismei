37b5ae30dd0e745e43680819508cf10857b74ec2
kuroi-ken <faznurramadhan2004@gmail.com>
2026-03-13 10:36:57 +0700

    separate admin sidebar menu to component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,28 @@
+    <div class="fixed h-screen w-2xs bg-blue-200 rounded-tr-2xl z-100 inset-0">
+        <div class="flex-col flex gap-3 pt-6">
+            <div class=" mx-auto w-3xs border-b border-black/20 pb-5">
+                {{-- <div class="flex justify-center mx-auto gap-3">
+                    <img src="../assets/seameo.png" class="size-20" alt="">
+                    <img src="../assets/seaqim.png" class="size-20" alt="">
+                </div> --}}
+                <h2 class="pt-2 text-center text-blue-900 font-medium text-[30px]">
+                    ISMEI 
+                </h2>
+                <h3 class="text-[18px] text-blue-900 text-center">
+                    Admin Panel
+                </h3>
+            </div>
+            <div class="px-5">
+                <h2 class="p-3 text-[15px] text-black/60">
+                    Navigations
+                </h2>
+                <ul class="flex flex-col gap-3">
+                    <li class="max-w-2xl hover:bg-blue-900 py-1 rounded-lg hover:text-[#F8FAFC] hover:pl-5 duration-300 text-blue-900 flex items-center"><i data-feather="home" class="w-5 h-5"></i><a class="pl-2" href="#">Home</a></li>
+                    <li class="max-w-2xl hover:bg-blue-900 py-1 rounded-lg hover:text-[#F8FAFC] hover:pl-5 duration-300 text-blue-900 flex items-center"><i data-feather="info" class="w-5 h-5"></i><a class="pl-2" href="#">Informations</a></li>
+                    <li class="max-w-2xl hover:bg-blue-900 py-1 rounded-lg hover:text-[#F8FAFC] hover:pl-5 duration-300 text-blue-900 flex items-center"><i data-feather="sunset" class="w-5 h-5"></i><a class="pl-2" href="#">Symposium</a></li>
+                    <li class="max-w-2xl hover:bg-blue-900 py-1 rounded-lg hover:text-[#F8FAFC] hover:pl-5 duration-300 text-blue-900 flex items-center"><i data-feather="archive" class="w-5 h-5"></i><a class="pl-2" href="#">Archives</a></li>
+                    <li class="max-w-2xl hover:bg-blue-900 py-1 rounded-lg hover:text-[#F8FAFC] hover:pl-5 duration-300 text-blue-900 flex items-center"><i data-feather="more-horizontal" class="w-5 h-5"></i><a class="pl-2" href="#">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
